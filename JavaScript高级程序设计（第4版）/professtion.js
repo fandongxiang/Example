38e8970a91bd00ad707db25a5bccc5f589503ec9
fandongxiang <730596730@qq.com>
2022-09-22 21:57:59 +0800

    2022-09-22

--- JavaScript高级程序设计（第4版）/professtion.js
+++ JavaScript高级程序设计（第4版）/professtion.js
@@ -966,5 +966,5 @@ let puller = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2', 'D1', 'D2', 'E1', 'E2'];
 // 数组归并
 let values = [1, 2, 3, 4, 5];
 console.log(values.reduce((prev, cur) => prev + cur)) // 15
-console.log(values.reduceRight((prev, cur) => prev + cur));
+console.log(values.reduceRight((prev, cur) => prev + cur)); // 15
 console.log(values.reduce((prev, cur) => prev * cur)) // 120
